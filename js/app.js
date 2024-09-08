a56e10fd40a26858d19356b631b65c065b7cc1d0
José Barraza Dávila <javier.barraza.d@mail.pucv.cl>
2024-09-07 20:32:07 -0400

    Corrección Footer Responsive

--- js/app.js
+++ js/app.js
@@ -3,4 +3,29 @@ $(function(){
     $("footer").load("components/footer.html");
     
 
+    
+    // LOGICA FOOTER RESPONSIVE
+
+    function aplicarFooterResponsive() {
+        const alturaPantalla = window.innerHeight;
+        const info = document.getElementById('info-footer');
+
+        if (alturaPantalla <= 700) {
+            info.classList = "contact-info row row-cols-2"
+        }else{
+            info.classList = "contact-info row row-cols-1"
+        }
+    }
+
+    setTimeout(()=>{
+        // Llamamos a la función al cargar la página
+        window.onload = aplicarFooterResponsive;
+        // También aplicamos la clase al redimensionar la ventana
+        window.onresize = aplicarFooterResponsive;
+    }, 500);
+
+
+
 });
+
+
