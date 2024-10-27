ee19c5263c7116a003eb6d6d6e91e373df4c7fd6
Javier Barraza <javier.barraza.d@mail.pucv.cl>
2024-10-27 13:37:02 -0300

    Proyecto ionic react

--- js/reservar-cita.js
+++
@@ -1,7 +0,0 @@
-const datepicker = document.getElementById('fecha');
-
-const fechaActual = new Date().toISOString().split('T')[0];
-
-console.log(fechaActual);
-
-datepicker.setAttribute('min', fechaActual);
---
+++ proyect-ingeweb/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proyect-ingeweb',
+  webDir: 'dist'
+};
+
+export default config;
